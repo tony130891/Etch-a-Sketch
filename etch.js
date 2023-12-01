4c96685bd41e9d0c15d365479978d450b7ba5bdf
Tony <antonivelasquez67@gmail.com>
2023-12-01 18:10:40 -0400

    added reset button

--- etch.js
+++ etch.js
@@ -2,7 +2,7 @@ const container = document.querySelector(".container");
 const btnInput = document.querySelector(".btnInput");
 
 const squares = 0;
-function row() {
+function row(squares = 20) {
   for (let i = 0; i < squares; i++) {
     let div = document.createElement("div");
     div.classList.add("row");
@@ -16,4 +16,12 @@ container.addEventListener("mouseover", (e) => {
 
 btnInput.addEventListener("click", () => {
   let input = prompt("how many squares do you want?");
+  if (input > 230) {
+    prompt("Squares must be numbers and not be greater than 230");
+  }
+  row(input);
 });
+
+function resetDiv() {
+  window.location.reload();
+}
