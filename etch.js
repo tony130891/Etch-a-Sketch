20154c4d5523c44db2559e7eed7e164792378588
Tony <antonivelasquez67@gmail.com>
2023-12-01 16:11:03 -0400

    Working on the input

--- etch.js
+++ etch.js
@@ -1,10 +1,19 @@
 const container = document.querySelector(".container");
+const btnInput = document.querySelector(".btnInput");
 
+const squares = 0;
 function row() {
-  for (let i = 0; i < 273; i++) {
+  for (let i = 0; i < squares; i++) {
     let div = document.createElement("div");
     div.classList.add("row");
     container.appendChild(div);
   }
 }
-row();
+
+container.addEventListener("mouseover", (e) => {
+  e.target.classList.replace("row", "changed");
+});
+
+btnInput.addEventListener("click", () => {
+  let input = prompt("how many squares do you want?");
+});
